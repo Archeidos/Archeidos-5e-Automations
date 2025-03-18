d3856f895dc3068beeae5bcbc0262fad0efefbb1
Archeidos <mbthomas007@gmail.com>
2025-03-18 01:35:17 -0400

    added test settings

---
+++ src/main.js
@@ -0,0 +1,16 @@
+export const MODULE_ID = "archeidos-automations";
+
+Hooks.once('init', () => {
+    console.log('archeidos-automations | Initializing my custom module');
+
+    // Register a custom setting
+    game.settings.register(MODULE_ID, 'mySetting', {
+        name: 'My Setting',
+        hint: 'A description of what this setting does.',
+        scope: 'world',
+        config: true,
+        type: String,
+        default: 'default value'
+    });
+
+})
